9ef82b7798d6b7cd79ca784f5eb311e1b57b4d06
Edo <muhammadridho5654@gmail.com>
2024-12-05 20:37:04 +0700

    feat: implement mvvm to more detail pitchdeck (mentor)  screen

--- app/src/main/java/com/example/slicingbcf/implementation/mentor/pitchdeck/PitchdeckConstant.kt
+++ app/src/main/java/com/example/slicingbcf/implementation/mentor/pitchdeck/PitchdeckConstant.kt
@@ -1,10 +1,26 @@
 package com.example.slicingbcf.implementation.mentor.pitchdeck
 
+import com.example.slicingbcf.data.local.LembarKerjaPeserta
 import com.example.slicingbcf.data.local.worksheetsPeserta
 
 class PitchdeckConstant {
   companion object {
      val mockUpPitchdeckMentor = worksheetsPeserta
+
+    val mockUpLembarKerjaPeserta = listOf(
+      LembarKerjaPeserta(
+        namaPeserta = "Asep",
+        waktuSubmisi = "Senin, 1 April 2024 13.55 WIB",
+      ),
+      LembarKerjaPeserta(
+        namaPeserta = "Budi",
+        waktuSubmisi = "Selasa, 2 April 2024 13.55 WIB",
+      ),
+      LembarKerjaPeserta(
+        namaPeserta = "Cecep",
+        waktuSubmisi = "Rabu, 3 April 2024 13.55 WIB",
+      ),
+    )
 
   }
 }
